a7bb8201bc7950469e73b44570a16084590ff7dd
cowtipping <turbogerbil@gmail.com>
2022-11-29 16:48:18 +0000

    Input and Textarea components and new tests

--- just-the-ticket/src/Components/NavBar/index.js
+++ just-the-ticket/src/Components/NavBar/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import logo from "../../just-the-ticket-nav-bar-03.png";
-import hamburger from "../../just-the-ticket-nav-bar-04.png";
+import logo from "../../assets/just-the-ticket-nav-bar-03.png";
+import hamburger from "../../assets/just-the-ticket-nav-bar-04.png";
 import "./NavBar.css";
 
 const NavBar = () => {
-    return(
-
-        <div className="nav-bar">
-            <img src={logo} className="logo" alt="Just the Ticket Logo"/>
-            <img src={hamburger} className="hamburger" alt="Mock Hamburger Menu"/>
-
-        </div> 
-
-    );
-}
+  return (
+    <div className="nav-bar">
+      <img src={logo} className="logo" alt="Just the Ticket Logo" />
+      <img src={hamburger} className="hamburger" alt="Mock Hamburger Menu" />
+    </div>
+  );
+};
 export default NavBar;
